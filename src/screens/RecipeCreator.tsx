844a325eba79a7d0673d21305362bcdb686b7787
Noémi PUSKÁS <n.puskas@adiumsoft.hu>
2022-10-23 15:14:06 +0200

    RecipeCreator added to App

---
+++ src/screens/RecipeCreator.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function RecipeCreator() {
+  return (
+    <>
+      <form>
+        <input type="text"></input>
+        <button type="submit">Beküld</button>
+      </form>
+    </>
+  );
+}
