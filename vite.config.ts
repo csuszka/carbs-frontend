66b4ab9b8830ca66220d3666b1fcf17249738459
csuszka <amifontos@gmail.com>
2023-04-27 06:21:13 +0200

    Upgrading node to avoid path error

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+// import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
