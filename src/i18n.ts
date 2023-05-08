3442ce1fe7f2b292e0480fa1e9f9c28bb9036c04
csuszka <amifontos@gmail.com>
2023-05-08 08:54:37 +0200

    translation file preparations

---
+++ src/i18n.ts
@@ -0,0 +1,23 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+i18next
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    debug: true,
+    fallbackLng: "en",
+    resources: {
+      en: {
+        translation: {
+          ingredient: "ingredient",
+        },
+      },
+      hu: {
+        translation: {
+          ingredient: "összetevő",
+        },
+      },
+    },
+  });
